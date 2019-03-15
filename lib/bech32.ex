689eed647fc4bfc0cf69492aa631274a170e5458
KaFai Choi <kafaicoder@gmail.com>
2019-03-15 22:34:05 +0100

    INITIAL COMMIT

---
+++ lib/bech32.ex
@@ -0,0 +1,18 @@
+defmodule Bech32 do
+  @moduledoc """
+  Documentation for Bech32.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bech32.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/bech32_test.exs
@@ -0,0 +1,8 @@
+defmodule Bech32Test do
+  use ExUnit.Case
+  doctest Bech32
+
+  test "greets the world" do
+    assert Bech32.hello() == :world
+  end
+end
