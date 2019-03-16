69013ddcbe131aa5411a0e4eff32c947f37da54f
KaFai Choi <kafaicoder@gmail.com>
2019-03-16 01:02:48 +0100

    add data_char_mapping for getting data from encoded string

--- lib/bech32.ex
+++ lib/bech32.ex
@@ -4,7 +4,6 @@ defmodule Bech32 do
   """
 
   @doc """
-  Hello world.
 
   ## Examples
 
@@ -15,8 +14,11 @@ defmodule Bech32 do
   use Bitwise
 
   @gen [0x3B6A57B2, 0x26508E6D, 0x1EA119FA, 0x3D4233DD, 0x2A1462B3]
+  @data_char_whitelist 'qpzry9x8gf2tvdw0s3jn54khce6mua7l'
+  @data_char_map Enum.zip(@data_char_whitelist, 0..Enum.count(@data_char_whitelist))
+                 |> Enum.into(%{})
 
-  def bech32_polymod(values) do
+  defp bech32_polymod(values) do
     Enum.reduce(
       values,
       1,
@@ -24,8 +26,8 @@ defmodule Bech32 do
         b = acc >>> 25
         acc = ((acc &&& 0x1FFFFFF) <<< 5) ^^^ value
 
-        Enum.reduce(0..4, acc, fn i, acc ->
-          acc ^^^
+        Enum.reduce(0..length(@gen), acc, fn i, in_acc ->
+          in_acc ^^^
             if (b >>> i &&& 1) != 0 do
               Enum.at(@gen, i)
             else
@@ -36,21 +38,23 @@ defmodule Bech32 do
     )
   end
 
-  defp bech32_hrp_expand(str) do
-    str_charlist = String.to_charlist(str)
-    x1 = Enum.map(str_charlist, &(&1 >>> 5))
-    x2 = Enum.map(str_charlist, &(&1 &&& 31))
+  defp bech32_hrp_expand(chars) do
+    x1 = Enum.map(chars, &(&1 >>> 5))
+    x2 = Enum.map(chars, &(&1 &&& 31))
     x1 ++ [0] ++ x2
   end
 
   @spec bech32_verify_checksum(binary()) :: bool
   def bech32_verify_checksum(bech32_str) do
-    {hrp, data} = split_bech32_str(bech32_str)
-    bech32_polymod(bech32_hrp_expand(hrp) ++ String.to_charlist(data)) == 1
+    {hrp, data} = bech32_str |> String.downcase() |> split_bech32_str()
+    checksum = bech32_polymod(bech32_hrp_expand(hrp) ++ data)
+    checksum == 1
   end
 
   defp split_bech32_str(str) do
     [hrp, data] = String.split(str, "1", parts: 2)
+    hrp = hrp |> String.to_charlist()
+    data = data |> String.to_charlist() |> Enum.map(&Map.get(@data_char_map, &1))
     {hrp, data}
   end
 end
