6abd6fbd621c5fab06ba5feb235f4214315784e0
KaFai Choi <kafaicoder@gmail.com>
2019-03-16 01:13:30 +0100

    fix split bech32 to hrp and data

--- lib/bech32.ex
+++ lib/bech32.ex
@@ -3,20 +3,26 @@ defmodule Bech32 do
   Documentation for Bech32.
   """
 
-  @doc """
-
-  ## Examples
-
-      iex>  Bech32.bech32_verify_checksum("A12UEL5L")
-      true
-
-  """
   use Bitwise
 
   @gen [0x3B6A57B2, 0x26508E6D, 0x1EA119FA, 0x3D4233DD, 0x2A1462B3]
   @data_char_whitelist 'qpzry9x8gf2tvdw0s3jn54khce6mua7l'
   @data_char_map Enum.zip(@data_char_whitelist, 0..Enum.count(@data_char_whitelist))
                  |> Enum.into(%{})
+
+  @doc """
+  ## Examples
+
+      iex>  Bech32.bech32_verify_checksum("A12UEL5L")
+      true
+
+  """
+  @spec bech32_verify_checksum(binary()) :: bool
+  def bech32_verify_checksum(bech32_str) do
+    {hrp, data} = bech32_str |> String.downcase() |> split_bech32_str()
+    checksum = bech32_polymod(bech32_hrp_expand(hrp) ++ data)
+    checksum == 1
+  end
 
   defp bech32_polymod(values) do
     Enum.reduce(
@@ -39,22 +45,19 @@ defmodule Bech32 do
   end
 
   defp bech32_hrp_expand(chars) do
-    x1 = Enum.map(chars, &(&1 >>> 5))
-    x2 = Enum.map(chars, &(&1 &&& 31))
-    x1 ++ [0] ++ x2
-  end
-
-  @spec bech32_verify_checksum(binary()) :: bool
-  def bech32_verify_checksum(bech32_str) do
-    {hrp, data} = bech32_str |> String.downcase() |> split_bech32_str()
-    checksum = bech32_polymod(bech32_hrp_expand(hrp) ++ data)
-    checksum == 1
+    Enum.map(chars, &(&1 >>> 5)) ++ [0 | Enum.map(chars, &(&1 &&& 31))]
   end
 
   defp split_bech32_str(str) do
-    [hrp, data] = String.split(str, "1", parts: 2)
-    hrp = hrp |> String.to_charlist()
-    data = data |> String.to_charlist() |> Enum.map(&Map.get(@data_char_map, &1))
+    # the bech 32 is at most 90 chars
+    # so it's ok to do 3 time reverse here
+    # otherwise we can use binary pattern matching with index for better performance
+    [data, hrp] = str |> String.reverse() |> String.split("1", parts: 2)
+    hrp = hrp |> String.reverse() |> String.to_charlist()
+
+    data =
+      data |> String.reverse() |> String.to_charlist() |> Enum.map(&Map.get(@data_char_map, &1))
+
     {hrp, data}
   end
 end
